334c0ac355f7460384c4ba591e45f066659bfbea
Supriya 007 <singhsupriya711@gmail.com>
2024-03-04 23:25:22 +0530

    components and routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { Suspense, lazy } from 'react'
+import Loader from './components/Loader'
+const Dashboard=lazy(()=> import('./pages/Dashboard'))
+const Products=lazy(()=> import('./pages/Products'))
+const Customer=lazy(()=> import('./pages/Customer'))
+const Transaction=lazy(()=> import('./pages/Transaction'))
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Suspense fallback={<Loader/>}>
+<Router>
+    <Routes>
+      <Route path='/admin/dashboard' element={<Dashboard/>}/>
+      <Route path='/admin/products' element={<Products/>}/>
+      <Route path='/admin/customer' element={<Customer/>}/>
+      <Route path='/admin/transaction' element={<Transaction/>}/>
+
+    </Routes>
+   </Router>
+    </Suspense>
+   
   )
 }
 
